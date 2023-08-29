7afa376966e9865a8b19cd5aab9f6f53b8eda63a
Alonzo Sanchez <alonzosanchez3@gmail.com>
2023-08-29 15:15:05 -0500

    updated state on song details

--- components/songDetails/SongDetails.tsx
+++ components/songDetails/SongDetails.tsx
@@ -1,14 +1,21 @@
+import { RootState } from "@/store/store";
 import React from "react";
+import { useSelector } from "react-redux";
 
 const SongDetails = () => {
+
+  const music = useSelector((state: RootState) => {
+    return state.music
+  })
+
   return (
     <div className="flex py-2 rounded-lg ">
       <div>
         <img className="rounded-[10%]" src="https://picsum.photos/100/100" />
       </div>
       <div className="flex flex-col justify-center ml-2">
-        <h3 className="text-3xl text-[#564d4d] font-bold">Clair De Lune</h3>
-        <p className=" text-[#959292]">Claude Debussy</p>
+        <h3 className="text-3xl text-[#564d4d] font-bold">{music.piece ? music.piece.name : ''}</h3>
+        <p className=" text-[#959292]">{music.piece ? music.piece.composer : ''}</p>
       </div>
     </div>
   );
